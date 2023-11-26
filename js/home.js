3d1cf3edf176cc3e807031af2ff669217ddb29e2
Benedict Avenido <benedictavenido1320@gmail.com>
2023-11-26 10:18:21 +0800

    [Carousel Functionality Added]

---
+++ js/home.js
@@ -0,0 +1,16 @@
+const slider = document.querySelector(".slider");
+
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+
+var sectionIndex = 0;
+
+rightArrow.addEventListener('click', function(){
+    sectionIndex = (sectionIndex < 2)? sectionIndex + 1: 2;
+    slider.style.transform = 'translate('+ (sectionIndex) * -33.3 + '%)';
+});
+
+leftArrow.addEventListener('click', function(){
+    sectionIndex = (sectionIndex > 0)? sectionIndex - 1: 0;
+    slider.style.transform = 'translate('+ (sectionIndex) * -33.3 + '%)';
+});
