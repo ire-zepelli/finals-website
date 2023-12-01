0d0cac66741e31575e7729ad8575b33fa726830d
Benedict Avenido <benedictavenido1320@gmail.com>
2023-12-02 00:01:22 +0800

    [80% done]

--- js/home.js
+++ js/home.js
@@ -25,15 +25,13 @@ function updateSelectedButton(newSelectedBtn) {
     );
     firstButton.classList.add("selected");
     selectedBtn = firstButton;
-  } 
-  else if (currentIndex === -1) {
+  } else if (currentIndex === -1) {
     let lastButton = document.querySelector(
       ".buttons > .slider-btn:last-child"
     );
-  lastButton.classList.add('selected');
-  selectedBtn = lastButton;
-  }
-  else {
+    lastButton.classList.add("selected");
+    selectedBtn = lastButton;
+  } else {
     newSelectedBtn.classList.add("selected");
     selectedBtn = newSelectedBtn;
   }
@@ -41,27 +39,75 @@ function updateSelectedButton(newSelectedBtn) {
 
 firstSliderBtn.addEventListener("click", function () {
   console.log("1 clicked");
-  selectedBtn.classList.remove("  selected");
+  slider.style.opacity = "0";
+  selectedBtn.classList.remove("selected");
   firstSliderBtn.classList.add("selected");
   selectedBtn = firstSliderBtn;
+  currentIndex = 0;
+  setTimeout(function () {
+    slider.innerHTML = `
+                    <section>
+                    <img class="featured-img" src="img/featured-sectiion/featured-1.jpg" alt="featured-img"/>
+                    </section>
+                    <section>
+                   <img class="featured-img" src="img/featured-sectiion/featured-2.jpg" alt="featured-img"/>
+                   </section>
+                   <section> 
+                   <img class="featured-img" src="img/featured-sectiion/featured-3.jpg" alt="featured-img"/>
+                   </section>
+                     `;
+    slider.style.opacity = "1";
+  }, 150);
 });
 
 secondSliderBtn.addEventListener("click", function () {
   console.log("2 clicked");
+  slider.style.opacity = "0";
   selectedBtn.classList.remove("selected");
   secondSliderBtn.classList.add("selected");
   selectedBtn = secondSliderBtn;
+  currentIndex = 1;
+  setTimeout(function () {
+    slider.style.opacity = "1";
+    slider.innerHTML = `
+                    <section>
+                   <img class="featured-img" src="img/featured-sectiion/featured-2.jpg" alt="featured-img"/>
+                   </section>
+                   <section> 
+                   <img class="featured-img" src="img/featured-sectiion/featured-3.jpg" alt="featured-img"/>
+                   </section>
+                   <section>
+                    <img class="featured-img" src="img/featured-sectiion/featured-1.jpg" alt="featured-img"/>
+                    </section>
+                     `;
+  }, 150);
 });
 
 thirdSliderBtn.addEventListener("click", function () {
   console.log("3 clicked");
+  slider.style.opacity = "0";
   selectedBtn.classList.remove("selected");
   thirdSliderBtn.classList.add("selected");
   selectedBtn = thirdSliderBtn;
+  currentIndex = 2;
+  setTimeout(function () {
+    slider.innerHTML = `
+    <section> 
+    <img class="featured-img" src="img/featured-sectiion/featured-3.jpg" alt="featured-img"/>
+    </section>
+    <section>
+     <img class="featured-img" src="img/featured-sectiion/featured-1.jpg" alt="featured-img"/>
+     </section>
+     <section>
+    <img class="featured-img" src="img/featured-sectiion/featured-2.jpg" alt="featured-img"/>
+    </section>
+      `;
+    slider.style.opacity = "1";
+  }, 150);
 });
 
 rightArrow.addEventListener("click", function () {
-    currentIndex++;
+  currentIndex++;
   slider.style.opacity = "0";
   console.log(currentIndex);
   setTimeout(function () {
@@ -70,19 +116,19 @@ rightArrow.addEventListener("click", function () {
     slider.style.opacity = "1";
   }, 150);
   updateSelectedButton(selectedBtn.nextElementSibling);
-  currentIndex = currentIndex > 2 ? 0: currentIndex;
+  currentIndex = currentIndex > 2 ? 0 : currentIndex;
 });
 
 leftArrow.addEventListener("click", function () {
-    currentIndex--;
-    slider.style.opacity = "0";
+  currentIndex--;
+  slider.style.opacity = "0";
   setTimeout(function () {
     let lastItem = document.querySelector(".slider section:last-child");
     slider.prepend(lastItem);
     slider.style.opacity = "1";
-  },150);
+  }, 150);
 
   console.log(currentIndex);
   updateSelectedButton(selectedBtn.previousElementSibling);
-  currentIndex = currentIndex < 0 ? 2: currentIndex;
+  currentIndex = currentIndex < 0 ? 2 : currentIndex;
 });
