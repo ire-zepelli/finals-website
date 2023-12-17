7b13fb94813fc6eb70760550535c46cc62bf96f4
Benedict Avenido <benedictavenido1320@gmail.com>
2023-12-17 16:42:27 +0800

    added media queries to login and fixed bugs

--- js/login.js
+++ js/login.js
@@ -1,13 +1,14 @@
-const loginPage = document.querySelector(".loginform");
-const registerPage = document.querySelector(".registerform");
+const loginForm = document.querySelector(".loginform");
+const registerForm = document.querySelector(".registerform");
+const loginBtn = document.getElementById("login-btn")
 
 document.getElementById('signup').addEventListener('click', function () {
-    loginPage.classList.add('hidden');
-    registerPage.classList.remove('hidden');
+    loginForm.classList.add('hidden');
+    registerForm.classList.remove('hidden');
     });
 document.getElementById('login').addEventListener('click', function () {
-    loginPage.classList.remove('hidden');
-    registerPage.classList.add('hidden');
+    loginForm.classList.remove('hidden');
+    registerForm.classList.add('hidden');
     });
 
 const eye1 = document.getElementById('eye1');
@@ -27,4 +28,6 @@ const password2 = document.getElementById('password2');
         eyeIcon.classList.toggle('fa-eye-slash');
     }
 
-
+loginBtn.addEventListener('click', ()=>{
+    window.location.href = "home.html";
+});
